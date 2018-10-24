af04db40da2c6d483c5533d9b38a00c4e867f177
Jim Sesser <sesser@Titan.HPC.MsState.Edu>
2018-10-24 12:56:52 -0500

    basic oldquiz class

---
+++ oldquiz.cpp
@@ -0,0 +1,23 @@
+#include<string> 
+#include<iostream> 
+#include<vector> 
+#include<fstream> 
+#include<regex> 
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include "oldquiz.h"
+using namespace std;
+
+
+	void printQuizes(){
+
+		ifstream infile;
+		infile.open("oldquiz.txt");
+		string line;
+
+
+	}
+
+	// old quizes are just an integer array of chapters and number of questions
+	void saveQuiz(int* c, int n);
---
+++ oldquiz.h
@@ -0,0 +1,11 @@
+using namespace std;
+
+class oldquiz {
+	public:
+	void printQuizes();
+
+	// old quizes are just an integer array of chapters and number of questions	
+	void saveQuiz(int* c, int n);
+};
+
+
