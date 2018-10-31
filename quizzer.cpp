c04deadc19460f3896aab9a17d012058837ef6cc
dannyd2 <dannydfpv@gmail.com>
2018-10-31 12:32:26 -0500

    quizzer.cpp

--- quizzer.cpp
+++ quizzer.cpp
@@ -27,7 +27,8 @@ int main () {
   // build bank of questions and old quiz formats
   Bank mainbank;
   oldquiz Oldquiz;
-
+  quizholder saved_test_ID; 
+   
   cout << "Would you like to take a predefined quiz or design your own?" << endl;
   cout << "1. Predefined " << endl; 
   cout << "2. Design my own " << endl; 
@@ -53,6 +54,7 @@ int main () {
 // build quiz if that was the option picked
 if(custom_quiz){ 
   // chapter selector 
+  cout << "Your ID for this quiz is " << saved_test_ID.getID() << ". Please take note of this value, you will use it to take this quiz again in the future." << endl;
   mainbank.printChapters();
   cout << "Please select chapters " << endl;
 
@@ -198,6 +200,8 @@ if(custom_quiz){
 
 	// save quiz. Conver chapters to an array reference
 	Oldquiz.saveQuiz(&Chapters[0],Chapters.size(),questions_to_take);
+    
+
 
 }
 
